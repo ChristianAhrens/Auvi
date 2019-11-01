606c1ca2545fbe43a94b7b8b30741379858b2f47
Christian Ahrens <christianahrens@me.com>
2019-11-01 16:39:00 +0100

    WIP layouting

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -18,12 +18,15 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
-    setSize (600, 400);
+    setSize (400, 600);
     
     m_Header            = std::make_unique<MainHeader>();
+	addAndMakeVisible(m_Header.get());
     m_AudioVisualizer   = std::make_unique<TwoDFieldAudioVisualizer>();
+	addAndMakeVisible(m_AudioVisualizer.get());
     m_Footer            = std::make_unique<MainFooter>();
-    
+	addAndMakeVisible(m_Footer.get());
+
     m_Processor         = std::make_unique<MainProcessor>();
 }
 
@@ -31,20 +34,31 @@ MainComponent::~MainComponent()
 {
 }
 
-//==============================================================================
 void MainComponent::paint (Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
-
-    g.setFont (Font (16.0f));
-    g.setColour (Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), Justification::centred, true);
+	// (Our component is opaque, so we must completely fill the background with a solid colour)
+	g.fillAll(getLookAndFeel().findColour(ResizableWindow::backgroundColourId));
 }
 
 void MainComponent::resized()
 {
-    // This is called when the MainComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.
+	auto panelMaxSize = 50.0f;
+	auto isPortrait = getLocalBounds().getHeight() > getLocalBounds().getWidth();
+
+	FlexBox fb;
+	if(isPortrait)
+	{
+		fb.flexDirection = FlexBox::Direction::column;
+		fb.items.addArray({ FlexItem(*m_Header.get()).withMaxWidth(panelMaxSize),
+							FlexItem(*m_AudioVisualizer.get()),
+							FlexItem(*m_Footer.get()).withMaxWidth(panelMaxSize) });
+	}
+	else
+	{
+		fb.flexDirection = FlexBox::Direction::row;
+		fb.items.addArray({ FlexItem(*m_Header.get()).withMaxHeight(panelMaxSize),
+							FlexItem(*m_AudioVisualizer.get()),
+							FlexItem(*m_Footer.get()).withMaxHeight(panelMaxSize) });
+	}
+	fb.performLayout(getLocalBounds().toFloat());
 }
--- Source/MainHeader.cpp
+++ Source/MainHeader.cpp
@@ -25,22 +25,9 @@ MainHeader::~MainHeader()
 
 void MainHeader::paint (Graphics& g)
 {
-    /* This demo code just fills the component's background and
-       draws some placeholder text to get you started.
-
-       You should replace everything in this method with your own
-       drawing code..
-    */
-
-    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));   // clear the background
-
-    g.setColour (Colours::grey);
-    g.drawRect (getLocalBounds(), 1);   // draw an outline around the component
-
-    g.setColour (Colours::white);
-    g.setFont (14.0f);
-    g.drawText ("MainHeader", getLocalBounds(),
-                Justification::centred, true);   // draw some placeholder text
+	//g.fillAll(getLookAndFeel().findColour(ResizableWindow::backgroundColourId).overlaidWith(
+	//	getLookAndFeel().findColour(ResizableWindow::backgroundColourId)));
+	g.fillAll(Colours::red);
 }
 
 void MainHeader::resized()
